6d377f93fe2d588a2a09f061ae8d01fa85f7c89b
Prashanta Kharel <prashanta.kharel@gmail.com>
2022-12-21 18:34:31 -0500

    added swap

---
+++ swap.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 28;
+    int y = 50;
+
+    printf("x is %i\n", x);
+    printf("y is %i\n", y);
+
+    //swapping
+    int temp = x;
+    x=y;
+    y=temp;
+    printf("x is now %i\n", x);
+    printf("y is now %i\n", y);
+}
