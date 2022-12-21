21fb6a2ffb947ed2dfb897f74ab4f66ea64e20df
Prashanta Kharel <prashanta.kharel@gmail.com>
2022-12-21 18:45:29 -0500

    conflict test

--- swap.c
+++ swap.c
@@ -12,7 +12,7 @@ int main(void)
     int temp = x;
     x=y;
     y=temp;
-    printf("x is now %i\n", x*5);
+    printf("x is now %i\n", x);
     printf("y is now %i\n", y);
     printf("done\n");
 }
